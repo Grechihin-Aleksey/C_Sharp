d0bca95f6645eab10c08408492a21cd553a1b114
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-06-23 19:27:25 +0300

    add lesson 1_4

---
+++ Lesson_1/HW/1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+
+while(count<=a) {
+  if (count % 2 == 0) Console.WriteLine(count);
+  count++;
+}
+
