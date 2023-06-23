84b3433aa1d79f4b9c87a098243e013099dc4047
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-06-23 10:37:40 +0300

    Classwork 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Write a namber: ");
+// string nam_str = Console.ReadLine()!;
+// int num = int.Parse(nam_str);
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num * num);
