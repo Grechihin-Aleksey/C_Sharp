d0b5d124f34da867791c6237ead885161d0b7a20
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-06-23 17:37:14 +0300

    add hw 1_1

---
+++ Lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a == b) Console.WriteLine("Числа равны");
+else if (a>b) Console.Write("max: " + a + " " + "min: " + b );
+else Console.WriteLine("min: " + a + " " + "max: " + b);
