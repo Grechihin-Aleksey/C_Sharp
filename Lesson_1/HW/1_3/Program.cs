aa3249eaf0c7b18355c6112f7b5c555ac900f718
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-06-23 18:46:19 +0300

    add lesson 1_3

---
+++ Lesson_1/HW/1_3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int b = a%2;
+if (b == 0) Console.WriteLine("Число: " + a + " " + "четное");
+else Console.WriteLine("Число: " + a + " " + "не четное");
+
