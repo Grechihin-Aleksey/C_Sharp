fb9cb537a997ac6855bfa0688b333f3312eb148d
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-06-27 13:36:08 +0300

    add hw1

---
+++ Lesson_2/2.3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void function (int num)
+{
+  if (num % 7 == 0 && num % 23 == 0){
+    Console.WriteLine("Число кратно");
+  }else {
+    Console.WriteLine("Число не кратно");
+  }
+}
+
+
+int numA = int.Parse(Console.ReadLine()!); 
+function(numA);
+
+
