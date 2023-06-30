69b8baa4567c3152fb53085d8a920623248e6be8
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-06-30 09:48:53 +0300

    Добавил проверку на пятизначное число

--- Lesson_3/HW/hw_1/Program.cs
+++ Lesson_3/HW/hw_1/Program.cs
@@ -3,10 +3,19 @@
 
 string function(int item)
 {
-  if (item / 10000 == item % 10 &&
-      item / 1000 % 10 == item / 10 % 10) return "Число полиндром";
+  if (item >= 10000 && item <= 99999)
+  {
+    if (item / 10000 == item % 10 &&
+        item / 1000 % 10 == item / 10 % 10) return "Число полиндром";
 
-  return "Число не полиндром";
+    return "Число не полиндром";
+  }
+  else
+  {
+    return "Число должно быть пятизначным: ";
+  }
 }
 
+Console.WriteLine("Введите пятизначное число: ");
 Console.WriteLine(function(int.Parse(Console.ReadLine()!)));
+
