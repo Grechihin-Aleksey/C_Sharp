27cbaca62b4d9d96dbaca1bf04e5813c04a0bc37
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-06-30 09:37:01 +0300

    add hw_3.1

---
+++ Lesson_3/HW/hw_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+string function(int item)
+{
+  if (item / 10000 == item % 10 &&
+      item / 1000 % 10 == item / 10 % 10) return "Число полиндром";
+
+  return "Число не полиндром";
+}
+
+Console.WriteLine(function(int.Parse(Console.ReadLine()!)));
