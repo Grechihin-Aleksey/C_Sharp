76953fc74cda116d6a5d9df0a8b27fab06e4453f
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-07-06 10:35:00 +0300

    add hw 4.3

---
+++ Lesson_4/HW/hw_3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Создать массив случайных чисел длиной N
+
+// void function()
+// {
+//   Console.WriteLine("Введите длину массива N: ");
+//   int[] Array = new int[int.Parse(Console.ReadLine()!)];
+
+//   Console.WriteLine("Введите диапазон массива, старт: ");
+//   int n = int.Parse(Console.ReadLine()!);
+
+//   Console.WriteLine("Введите диапазон массива, конец: ");
+//   int m = int.Parse(Console.ReadLine()!);
+
+//   for (int i = 0; i < Array.Length; i++)
+//   {
+//     Array[i] = new Random().Next(n, m);
+//     Console.Write(Array[i] + " ");
+//   }
+// }
+
+// function();
+
+Console.WriteLine("Введите длину массива N: ");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите диапазон массива, старт: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите диапазон массива, конец: ");
+int m = int.Parse(Console.ReadLine()!);
+
+
+void Array(int[] collection)
+{
+  for (int index = 0; index < collection.Length; index++)
+  {
+    collection[index] = new Random().Next(n, m);
+  }
+}
+
+void PrintArray(int[] col)
+{
+  for (int i = 0; i < col.Length; i++)
+  {
+    Console.Write(col[i] + " ");
+  }
+}
+
+int[] array = new int[num];
+
+
+
+
+
+Array(array);
+PrintArray(array);
