e501e55ffe015e942a57623c380a2b492ab8d095
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-07-06 08:59:05 +0300

    add hw 4_1

---
+++ Lesson_4/HW/hw_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который на входе принимает два числа А и В
+// и возводит число А в степень В
+
+Console.WriteLine("Введите число A: ");
+Console.WriteLine("Введите число B: ");
+
+void function(int A, int B)
+{
+  int rez = 1;
+  if (B == 0) Console.WriteLine($"Число В равно нулю -> {rez}");
+  else
+  {
+    for (int i = 1; i <= B; i++)
+    {
+      rez = rez * A;
+    }
+    Console.WriteLine($"Число А в степени В равно -> {rez}");
+  }
+}
+
+
+
+function(int.Parse(Console.ReadLine()!), int.Parse(Console.ReadLine()!));
