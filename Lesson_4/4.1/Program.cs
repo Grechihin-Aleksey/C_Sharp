1f23968c8db7cc3a0db1d39c1881cdb5a2b1dfb4
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-07-06 07:11:45 +0300

    add 4.1

---
+++ Lesson_4/4.1/Program.cs
@@ -0,0 +1,13 @@
+﻿int function (int num)
+{
+  int rez = 0;
+  for (int i = 1; num != 0; i++)
+  {
+    num /= 10;
+    rez = i;
+  }
+  return rez;
+}
+
+Console.WriteLine("Введите число N: ");
+Console.WriteLine(function(int.Parse(Console.ReadLine()!)));
