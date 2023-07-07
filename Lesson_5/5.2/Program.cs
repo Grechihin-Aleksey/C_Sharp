ff4fa1ed9c8733a82348eb9d731045c364d34b8a
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-07-07 21:45:09 +0300

    add lesson 5.2

---
+++ Lesson_5/5.2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Присутствует ли заданное число пользователем в массие
+
+Console.WriteLine("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите диапазон старта: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите введите финиш массива: ");
+int k = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число для сравнения: ");
+int Num = int.Parse(Console.ReadLine()!);
+
+int[] Array (int lenght, int start, int end)
+{
+  int[] mass = new int[lenght];
+  for (int i = 0; i < mass.Length; i++)
+  {
+    mass[i] = new Random().Next(start, end);
+  }
+  return mass;
+  Console.WriteLine($"Созданный массив -> {mass}");
+}
+
+void arrNew (int[] randomMass)
+{
+  for (int i = 0; i < randomMass.Length; i++) 
+  Console.Write(randomMass[i] + " ");
+} 
+
+string printArray (int[] col, int Num)
+{
+  for (int j = 0; j < col.Length; j++) 
+  {
+    if (col[j] == Num) return "Yes";
+  }
+  return "No";
+}
+
+
+
+int[] newArray = Array (n, m, k);
+Console.WriteLine(printArray (newArray, Num));
+arrNew (newArray);
+
