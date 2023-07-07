193aefb7e421c2b45553ec969ecc3efd9a12e2b5
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-07-07 23:01:43 +0300

    add lesson 5.3

---
+++ Lesson_5/5.3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив из 123 случайных чисел.
+//  Найти колличество числе, лежащих в отрезке от 10 до 99.
+
+int[] Array(int num)
+{
+  int[] arr = new int[num];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = new Random().Next(0, 200);
+    Console.Write(arr[i] + " ");
+  }
+  return arr;
+}
+
+int printArr(int[] col)
+{
+  int sam = 0;
+  for (int j = 0; j < col.Length; j++)
+  {
+    if (col[j] > 10 && col[j] < 99) sam += 1;
+  }
+  return sam;
+}
+
+int[] newArray = Array(123);
+Console.WriteLine("");
+Console.WriteLine("Элементов: " + printArr(newArray) + 
+" находящихся в диапазоне");
+
+
