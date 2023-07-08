39b60190ebbeba29bdc557f83fb7f79da798eaf4
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-07-08 21:29:58 +0300

    add hw_5.1

---
+++ Lesson_5/HW/hw_1/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] MassNums (int size, int from, int to)
+{
+ int[] arr = new int[size];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = new Random().Next(from, to);
+  }
+  return arr;
+}
+
+void PrintArray (int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write(arr[i] + " ");
+    if (arr[i] % 2 == 0) count = count + 1;
+  }
+  Console.WriteLine();
+  Console.Write("Колличество четных чисел в массиве -> " + count);
+}
+
+
+int[] mass = MassNums (8, 100, 1000);
+PrintArray (mass);
