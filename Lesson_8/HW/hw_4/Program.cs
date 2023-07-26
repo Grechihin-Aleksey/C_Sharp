6033bc1b8e78a94237fbfabdf43332a5df1c2587
Grechihin-Aleksey <9108921170@dz-led.ru>
2023-07-26 09:51:47 +0300

    Исправил задание

--- Lesson_8/HW/hw_4/Program.cs
+++ Lesson_8/HW/hw_4/Program.cs
@@ -7,7 +7,6 @@ Console.Write("Кол-во столбцов ");
 int col = int.Parse(Console.ReadLine()!);
 int[,] matrix = new int[row, col];
 
-
 Console.Write("Введите минимальное значение элемента массива : ");
 int min = int.Parse(Console.ReadLine()!);
 Console.Write("Введите максимальное значение элемента массива : ");
